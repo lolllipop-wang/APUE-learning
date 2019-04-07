69d2e2a91668a9f41d898e5662e0504b45b2fbb2
Wang Yihan <wang1998tom@outlook.com>
2019-04-07 16:59:03 +0800

    修复了自己实现的setenv和unsetenv函数中的BUG

--- src/process/my_setenv_and_unsetenv.c
+++ src/process/my_setenv_and_unsetenv.c
@@ -11,10 +11,13 @@ int my_unsetenv(const char *name);
 int
 main(int argc, char *argv[])
 {
+    /* 
+    方便调试
     if(clearenv() != 0) {
         perror("clearenv error");
         exit(EXIT_FAILURE);
     }
+    */
     if(my_setenv("SCHOOL", "XAUT", 1) == 0) {
         printf("my_setenv successed!\n");
     }
@@ -24,7 +27,7 @@ main(int argc, char *argv[])
     }
 
     if(my_unsetenv("SCHOOL") == 0)
-        printf("my_unsetenv successed\n");
+        printf("my_unsetenv successed!\n");
     else {
         printf("my_unsetenv error\n");
         exit(EXIT_FAILURE);
@@ -42,12 +45,13 @@ my_setenv(const char *name, const char *value, int overwrite)
     int name_length  = strlen(name);
     int value_length = strlen(value);
     int i;
-
+    /* 为新的环境变量申请空间 */
     new_env = (char *)malloc(sizeof(char) * (name_length + value_length + 2));
 
     if(new_env == NULL) {
         return -1;
     } 
+    /* 将name, value中的内容复制到新的空间中，中间使用'='分隔 */
     /* Copy begin */
     p = name;
     q = new_env;
@@ -60,12 +64,19 @@ my_setenv(const char *name, const char *value, int overwrite)
         *q++ = *p++;
     *q = 0;
     /* Copy end */
+
+    /* 如果environ为NULL，则直接加入新的环境变量 */
     if(environ == NULL || *environ == NULL) {
         return putenv(new_env);
     }
+
+
     if(overwrite == 0) {
+        /* overwrite 为 0 的情况 */
         char **ep;
         char *env_ptr = NULL;
+
+        /* 在环境变量中寻找要插入的变量是否已经存在 */
         for(ep = environ; *ep != NULL; ++ep) {
             if(strcmp(*ep, new_env) == 0) {
                 env_ptr = *ep;
@@ -73,14 +84,18 @@ my_setenv(const char *name, const char *value, int overwrite)
             }
         }
 
+        /* 不存在则加入， 存在直接返回 */
         if(env_ptr == NULL)
             return putenv(new_env);
         else 
             return 0;
     }
     else {
+        /* overwrite 为 1 的情况 */
         char **ep;
         char *env_ptr = NULL;
+
+        /* 查询新加入的变量是否已经存在， 如果存在就是用新的变量替换 */
         for(ep = environ; *ep != NULL; ++ep) {
             if(strcmp(*ep, new_env) == 0) {
                 *ep = new_env;
@@ -88,6 +103,7 @@ my_setenv(const char *name, const char *value, int overwrite)
                 break;
             }
         }
+        /* 不存在就加入 */
         if(env_ptr == NULL)
             return putenv(new_env);
         else 
@@ -106,28 +122,37 @@ my_unsetenv(const char *name)
     char **ep;
     char *env_ptr;
     char buf[1000];
+
+    /* 在环境变量中寻找要删除的变量 */
     for(ep = environ; *ep != NULL; ++ep) {
         env_ptr = *ep;
         char *p = buf;
         char *q = env_ptr;
-        while(*q != '=')
+        
+        /* 获取当前这一变量的 name */
+        while(q != NULL && *q != '=')
             *p++ = *q++;
         *p = 0;
 
+        /* 判断当前变量是否为要删除的变量 */
         if(strcmp(buf, name) == 0) {
             flag = 1;
             char **ep2 = ep + 1;
+
+            /* 将后面的变量向前移动 */
             while(*ep2 != NULL) {
                 *(ep2 - 1) = *ep2;
                 ep2++;
             }
+            *(ep2 - 1) = NULL;
+            /* 删除当前变量 */
+            free(env_ptr);
         }
-        free(env_ptr);
-        ep++;
 
     }
 
-    if(flag == 1) 
-        return 0;
-    return -1;
+    if(flag == 0)
+        return -1;
+
+    return 0;
 }
