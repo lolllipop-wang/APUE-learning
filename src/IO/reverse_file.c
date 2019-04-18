c7dae8532cd4a7b1735b6de19010d9fe87279ac7
Wang Yihan <wang1998tom@outlook.com>
2019-04-18 18:19:08 +0800

    更新面试时面试官提出的问题

--- src/IO/reverse_file.c
+++ src/IO/reverse_file.c
@@ -1,7 +1,8 @@
 /**
  * 
  * 面试的时候被面试官问到的题
- * 将一个大文件翻转，直接使用lseek，甚至栈都不需要，只需要一个缓冲区
+ * 将一个大文件翻转，直接使用lseek，甚至栈都不需要，只需要一个缓冲区。
+ * 
  */
 
 #include <apue.h>
@@ -14,7 +15,6 @@
 
 char buf[MAXBUFFSIZE];
 
-void reversebuf(char *buf, int length);  /* useless */
 int  read_line(int fd, char *buf, int max_length);
 
 int main(int argc, char *argv[])
@@ -22,15 +22,20 @@ int main(int argc, char *argv[])
     if(argc != 3) 
         err_quit("usage: %s input_file output_file", argv[0]);
     
-    int input_fd, output_fd, sub_fd[SUBFILENUMS];
-    int num_read, num_write;
-    off_t offset, input_file_length;
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+    int input_fd, output_fd;    /* 输入和输出文件的文件描述符 */
+    int num_read, num_write;    /* 每次正确读写的字节数      */
+    int input_file_length;      /* 输入文件的长度           */
 
-    if((input_fd = open(argv[1], O_RDONLY)) < 0)                    /* open input file */
+    /* 指定新建文件的权限 */
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;  
+
+    if((input_fd = open(argv[1], O_RDONLY)) < 0)                               /* 打开输入文件 */
         err_sys("open input file error");
 
-    if((output_fd = open(argv[2], O_RDWR | O_CREAT, mode)) < 0)     /* open output file */
+    /**
+     * 如果输出文件不存在，则创建该文件
+     */
+    if((output_fd = open(argv[2], O_RDWR | O_CREAT, mode)) < 0)                 /* 打开输出文件 */
         err_sys("open output file error");
 
     if((input_file_length = lseek(input_fd, 0, SEEK_END)) < 0)      
@@ -57,22 +62,6 @@ int main(int argc, char *argv[])
             err_sys("seek error");
     }
     exit(0);
-}
-
-void reversebuf(char *buf, int length)  /* useless */
-{
-    if(buf == NULL || length == 0)
-        return;
-    
-    char *p = buf, *q = buf + length - 1, c;
-
-    while(p <= q) {
-        c = *p;
-        *p = *q;
-        *q = c;
-        *p++;
-        *q--;
-    }
 }
 
 int read_line(int fd, char *buf, int max_length)
