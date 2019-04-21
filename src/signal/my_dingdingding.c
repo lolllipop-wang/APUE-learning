663ea75a24a26d37683748022c65e441c882e39e
Wang Yihan <wang1998tom@outlook.com>
2019-04-21 16:05:14 +0800

    添加定时器的几个简单练习

---
+++ src/signal/my_dingdingding.c
@@ -0,0 +1,65 @@
+/**
+ * 定时器与休眠
+ * 每隔sec秒叮叮叮。。。叮叮叮一次，共num次结束。
+ */
+#include <apue.h>
+#include <time.h>
+#include <sys/time.h>
+
+
+static volatile sig_atomic_t got_alarm = 0;
+
+static void handler(int sig)
+{
+    got_alarm = 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+        err_quit("usage: %s sec num");
+    
+    int              max_sigs;
+    int              sig_count;
+    clock_t          perv_clock;
+    struct sigaction sa;
+    struct itimerval itv;
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = handler;
+
+    if(sigaction(SIGALRM, &sa, NULL) < 0)
+        err_sys("setting SIGALRM handler error");
+    
+    itv.it_value.tv_sec = atoi(argv[1]);
+    itv.it_value.tv_usec = 0;
+    itv.it_interval.tv_sec = 1;
+    itv.it_interval.tv_usec = 0;
+
+    if(setitimer(ITIMER_REAL, &itv, NULL) == -1)
+        err_sys("setitmer error");
+
+    max_sigs = atoi(argv[2]);
+    sig_count = 0;
+    perv_clock = clock();
+    printf("Start: sec = %d, nums = %d\n", itv.it_value.tv_sec, max_sigs);
+
+    for(;;) {
+        while(((clock() - perv_clock) * 10 / CLOCKS_PER_SEC) < 5) {
+            if(got_alarm) {
+                got_alarm = 0;
+                printf("Alarm: 叮叮叮。。。叮叮叮\n");
+                sig_count++;
+                if(sig_count >= max_sigs) {
+                    printf("时间到了！\n");
+                    exit(0);
+                }
+            }
+        }
+        perv_clock = clock();
+        printf("Waitting。。。\n");
+    }
+
+    exit(0);
+}
---
+++ src/signal/timed_read.c
@@ -0,0 +1,56 @@
+/**
+ * 定时器与休眠
+ * 为read设置超时，如果read阻塞时间超过设定时间，则会中断read
+ */
+#include <apue.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#define MAXBUFFSIZE 4096
+
+char buf[MAXBUFFSIZE];
+
+static void handler(int sig)
+{
+    printf("\ncaught a signal\n");
+}
+
+int main(void)
+{
+    struct sigaction sa;
+    int num_read;
+    int time_out = 3;
+    int saved_errno;
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = handler;
+
+    if(sigaction(SIGALRM, &sa, NULL) == -1)
+        err_sys("setting SIGALRM handler error");
+    
+    printf("please enter a string, or it will time out int 3 seconds: ");
+    fflush(stdout);
+
+    alarm(time_out);
+    num_read = read(STDIN_FILENO, buf, MAXBUFFSIZE - 1);
+
+    saved_errno = errno;
+    alarm(0);
+    errno = saved_errno;
+
+    if(num_read == -1) {
+        if(errno == EINTR)
+            printf("read time out\n");
+        else 
+            err_sys("read error");
+    }
+    else {
+        printf("successful read %d bytes\n", num_read);
+        buf[num_read] = '\0';
+        printf("\tresult is: %s\n", buf);
+    }
+
+    exit(0);
+}
