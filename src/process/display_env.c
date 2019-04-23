29c0a893cbbfc52449d3a3b9e40274fd9dae5b96
Wang Yihan <wang1998tom@outlook.com>
2019-04-24 00:00:18 +0800

    添加了第一个exec函数的练习

--- src/process/display_env.c
+++ src/process/display_env.c
@@ -5,7 +5,7 @@
 extern char **environ;
 
 int
-main(int argc, char *argv)
+main(int argc, char *argv[])
 {
     char **ep;
     for(ep = environ; *ep != NULL; ++ep)
---
+++ src/process/t_execl.c
@@ -0,0 +1,27 @@
+#include <apue.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid;
+    int status;
+    switch ((pid = fork()))
+    {
+        case -1:
+            err_sys("fork error");
+            break;
+        case 0:
+            status = execl("/usr/bin/uname", "uname", "-a", (char *)0);
+            if(status < 0)
+                err_sys("execl failed");
+            
+        default:
+
+            if(waitpid(pid, &status, 0) < 0)
+                err_sys("waitpid error");
+            
+            pr_exit(status);
+            break;
+    }
+    exit(0);
+}
