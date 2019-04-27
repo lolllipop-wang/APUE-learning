5eac9334897fca478b2e3ca01e738603407a5324
Wang Yihan <wang1998tom@outlook.com>
2019-04-27 15:56:49 +0800

    使用子进程做过滤器处理输入

---
+++ src/pipe/character_filter.c
@@ -0,0 +1,21 @@
+/**
+ * 将输入的字符中的小写字符转换为大写，丢弃除数字和字母之外的特殊字符。
+ * cc -o character_filter character_filter.c -lapue
+ */
+#include <apue.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    int c;
+    while((c = getchar()) != EOF) {
+        
+        if(!isupper(c))
+            c = toupper(c);
+        if(putchar(c) == EOF)
+            err_sys("%s: output error", argv[0]);
+        if(c == '\n')
+            fflush(stdout);
+    }
+    exit(0);
+}
